4022bb7635992d0fc4a10bb3c9e4361208e3bca9
Israel Oriente <80863481+israeloriente1@users.noreply.github.com>
2022-08-03 20:30:39 -0300

    Novas adições
    - Adição de erro caso o input esteja com valor inválido.
    - Adição de função para remover mensagem de validação do input.
    - Correção de margin no footer
    - Configurações CSS na classe ".socialNetworks"

--- js/elementsInteraction.js
+++ js/elementsInteraction.js
@@ -1,24 +1,36 @@
+const form = document.querySelector("form");
+const formInput = document.querySelector("input#userURL");
+
+function disableValidationMsg(event) {
+    event.preventDefault();
+}
+
 function mobileMenu(event) {
     event.preventDefault();
     let mobileMenu = document.querySelector("section#menuContent");
     mobileMenu.classList.toggle("hidden");
 }
 
-
 async function verification(event) {  
     /* It will check if the input value is valid and return a new element with the shortened url */
-    event.preventDefault();
-    let urlButton = event.path[0];
-    let urlInput = event.path[1][0];
-    let isValid = urlInput.validity.valid;
-    let urlListContainer = document.querySelector("section.urlList");
 
-    urlButton.setAttribute("disabled", "true");
-    urlButton.innerHTML = `<span class="buttonLoading"></span>`;
+    event.preventDefault();
+    const formButton = event.path[0];
+    const urlListContainer = document.querySelector("section.urlList");
+    let isValid = formInput.validity.valid;
+
+    formButton.setAttribute("disabled", "true");
+    formButton.innerHTML = `<span class="buttonLoading"></span>`;
 
     if (isValid) {
-        
-        const apiRequest = await fetch(`https://api.shrtco.de/v2/shorten?url=${urlInput.value}`);
+
+        if (form.children.length == 3) {
+            formInput.style.outline = "none";
+            let span = form.children[1];
+            form.removeChild(span);
+        }
+
+        const apiRequest = await fetch(`https://api.shrtco.de/v2/shorten?url=${formInput.value}`);
         const apiReturn = await apiRequest.json();
 
         if (apiReturn.ok) {
@@ -39,18 +51,25 @@ async function verification(event) {
                 urlListContainer.removeChild(urlListContainer.children[i]);
             }, 6000)
         }
-    }else {
-        /* If the input value is not valid */
+    }else { /* If the url is not valid */
+        if (form.children.length == 2) {
+            formInput.style.outline = "2px solid var(--redColor)";
+            let span = document.createElement("span");
+            span.setAttribute("class", "validateMsg");
+            span.innerHTML = "Enter a valid url";
+
+            let lastChild = form.children[form.children.length - 1];
+            form.insertBefore(span, lastChild);
+        }
     }
 
     if (urlListContainer.children.length == 5) {
         urlListContainer.removeChild(urlListContainer.children[0]);
     }
 
-    urlInput.value = "";
-    urlButton.innerHTML = "Shorten it!";
-    urlButton.removeAttribute("disabled");
-    console.log(event);
+    formInput.value = "";
+    formButton.innerHTML = "Shorten it!";
+    formButton.removeAttribute("disabled");
 }
 
 
